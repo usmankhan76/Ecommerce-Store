313e872e14ee560be60c62fbb428d9e3e83bae25
usmankhan76 <72705472+usmankhan76@users.noreply.github.com>
2023-09-03 06:40:13 +0500

    pushing project

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use(cors())
 
 if(process.env.NODE_ENV==="production"){ 
     
-    app.use(enforce.HTTPS({trustProtoHeader:true}))
+    // app.use(enforce.HTTPS({trustProtoHeader:true}))
     app.use(express.static(path.join(__dirname,'../client/build')))
     app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
